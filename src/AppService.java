1b1dd36d67cd462d76f2049deb54bb4e276b4f37
Donggyu <98671555+ehdrb92@users.noreply.github.com>
2024-12-11 21:06:04 +0900

    add AppService Expense CRUD

--- src/AppService.java
+++ src/AppService.java
@@ -6,6 +6,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class AppService {
@@ -43,7 +44,7 @@ public class AppService {
                 expenses.add(new Expense(id, convertedDate, description, amount));
             }
         } catch (IOException e) {
-
+            System.out.println(e.getMessage());
         }
     }
 
@@ -60,5 +61,71 @@ public class AppService {
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    public void createExpense(Expense expense) {
+        expenses.add(expense);
+        System.out.println("Expense added successfully" + "(ID " + expense.getId() + ")");
+    }
+
+    public void getExpenses() {
+        System.out.println("ID  Date      Description  Amount");
+        for (Expense expense: expenses) {
+            System.out.print(expense.getId());
+            System.out.print(expense.getDate().toString());
+            System.out.print(expense.getDescription());
+            System.out.print("$" + expense.getAmount());
+            System.out.println();
+        }
+        return;
+    }
+
+    public void updateExpenseDate(int id, String date) {
+        for (Expense expense: expenses) {
+            if (id == expense.getId()) {
+                expense.setDate(commonUtil.convertStringToLocalDate(date));
+                System.out.println("Expense updated successfully" + "(ID " + id + ")");
+                return;
+            }
+        }
+        System.out.println("Not found expense" + "(ID " + id + ")");
+        return;
+    }
+
+    public void updateExpenseDescription(int id, String description) {
+        for (Expense expense: expenses) {
+            if (id == expense.getId()) {
+                expense.setDescription(description);
+                System.out.println("Expense updated successfully" + "(ID " + id + ")");
+                return;
+            }
+        }
+        System.out.println("Not found expense" + "(ID " + id + ")");
+        return;
+    }
+
+    public void updateExpenseAmount(int id, int amount) {
+        for (Expense expense: expenses) {
+            if (id == expense.getId()) {
+                expense.setAmount(amount);
+                System.out.println("Expense updated successfully" + "(ID " + id + ")");
+                return;
+            }
+        }
+        System.out.println("Not found expense" + "(ID " + id + ")");
+        return;
+    }
+
+    public void deleteExpense(int id) {
+        Iterator<Expense> iterator = expenses.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().getId() == 1) {
+                iterator.remove();
+                System.out.println("Expense deleted successfully" + "(ID " + id + ")");
+                return;
+            }
+        }
+        System.out.println("Not found expense" + "(ID " + id + ")");
+        return;
     }
 }
